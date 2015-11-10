39f7d0706543f20d70bc063067197b0088f3f25b
yize <yize.shc@gmail.com>
2015-11-10 20:12:07 +0800

    圆形

--- EZTest/CircleView.m
+++ EZTest/CircleView.m
@@ -20,6 +20,9 @@
     center.y = bounds.origin.y + bounds.size.height/2;
     float radius = (MIN(bounds.size.width, bounds.size.height)/2);
     
+    UIBezierPath *path = [[UIBezierPath alloc]init];
+    [path addArcWithCenter:center radius:radius startAngle:0.0 endAngle:M_PI*2.0 clockwise:YES];
+    [path stroke];
 }
 
 @end
