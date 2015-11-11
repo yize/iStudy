5e80fb6675502a532427bf32f47bb2cc510b759d
yize <yize.shc@gmail.com>
2015-11-11 22:28:39 +0800

    notification

--- HypnoNerd/HypnoNerd/BNRReminderViewController.m
+++ HypnoNerd/HypnoNerd/BNRReminderViewController.m
@@ -18,6 +18,10 @@
 {
     NSDate *date = self.datePicker.date;
     NSLog(@"Setting a reminder for %@",date);
+    UILocalNotification *note = [[UILocalNotification alloc]init];
+    note.alertBody = @"Hypnotize me!";
+    note.fireDate = date;
+    [[UIApplication sharedApplication] scheduleLocalNotification:note];
 }
 
 
