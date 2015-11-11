4c22db7498e6db580ab998c950e0f3dfbbd1df0f
yize <yize.shc@gmail.com>
2015-11-11 17:43:03 +0800

    新建新项目

---
+++ HypnoNerd/HypnoNerd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HypnoNerd
+//
+//  Created by yize on 15/11/11.
+//  Copyright © 2015年 yize. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
